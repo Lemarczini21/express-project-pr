31721f9db6c57f0a4b2abc0bb8991a39100622e7
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-04-26 12:31:33 +0200

    Add form and basic validation

--- server.js
+++ server.js
@@ -7,13 +7,21 @@ app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.post('/contact/send-message', (req, res) => {
-  res.json(req.body);
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message) {
+    res.send('The message has been sent!');
+  } else {
+    res.send("You can't leave fields empty!");
+  }
 });
 
 app.get('/hello/:name', (req, res) => {
